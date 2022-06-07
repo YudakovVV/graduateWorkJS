dbbd8559a8209a739c3b8f8ea535471270dcbeb4
YudakovVV <skiff2140@gmail.com>
2022-06-08 00:23:58 +0400

    create menu

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import modalHandler from "./modules/modalHandler"
-import modalHandlers from "./modules/modalHandlers"
+//import modalHandler from "./modules/modalHandler"
+//import modalHandlers from "./modules/modalHandlers"
 
-//import menu from "./modules/menu"
-import forms from "./modules/forms"
+import menu from "./modules/menu"
+//import forms from "./modules/forms"
 // import { sendForm } from "./modules/sendForm"
 //import slider from "./modules/slider"
 //import scrollUp from "./modules/scrollUp"
 
 
-modalHandler('.mobile-menu .callback-btn', 'block')
-modalHandler('.header .callback-btn', 'block')
-modalHandler('.modal-close', 'none')
-modalHandler('.quest-section .button-services', 'block')
-modalHandlers('.services-carousel .element', 'block')
-//menu()
-forms()
+//modalHandler('.mobile-menu .callback-btn', 'block')
+//modalHandler('.header .callback-btn', 'block')
+//modalHandler('.modal-close', 'none')
+//modalHandler('.quest-section .button-services', 'block')
+//modalHandlers('.services-carousel .element', 'block')
+menu()
+//forms()
 // sendForm()
 //slider()
 //scrollUp()
---
+++ src/modules/menu.js
@@ -0,0 +1,15 @@
+const menu = (e) => {
+    const smoothLinks = document.querySelectorAll('a[href^="#"]')
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href')
+
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
+}
+export default menu
