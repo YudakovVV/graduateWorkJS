840100074975762c0e39b0b0da476c10f2dcb17f
YudakovVV <skiff2140@gmail.com>
2022-06-08 00:20:03 +0400

    create form

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import modalHandler from "./modules/modalHandler"
 import modalHandlers from "./modules/modalHandlers"
 
-import menu from "./modules/menu"
+//import menu from "./modules/menu"
 import forms from "./modules/forms"
 // import { sendForm } from "./modules/sendForm"
-import slider from "./modules/slider"
-import scrollUp from "./modules/scrollUp"
+//import slider from "./modules/slider"
+//import scrollUp from "./modules/scrollUp"
 
 
-// modalHandler('.mobile-menu .callback-btn', 'block')
+modalHandler('.mobile-menu .callback-btn', 'block')
 modalHandler('.header .callback-btn', 'block')
 modalHandler('.modal-close', 'none')
 modalHandler('.quest-section .button-services', 'block')
 modalHandlers('.services-carousel .element', 'block')
-menu()
+//menu()
 forms()
 // sendForm()
-slider()
-scrollUp()
+//slider()
+//scrollUp()
 
