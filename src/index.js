4cb7bce7ddcd9a27df03839360c5e1eb962dcc0b
YudakovVV <skiff2140@gmail.com>
2022-06-08 00:26:47 +0400

    create scrollup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 //import modalHandler from "./modules/modalHandler"
 //import modalHandlers from "./modules/modalHandlers"
 
-import menu from "./modules/menu"
+//import menu from "./modules/menu"
 //import forms from "./modules/forms"
 // import { sendForm } from "./modules/sendForm"
 //import slider from "./modules/slider"
-//import scrollUp from "./modules/scrollUp"
+import scrollUp from "./modules/scrollUp"
 
 
 //modalHandler('.mobile-menu .callback-btn', 'block')
@@ -13,9 +13,9 @@ import menu from "./modules/menu"
 //modalHandler('.modal-close', 'none')
 //modalHandler('.quest-section .button-services', 'block')
 //modalHandlers('.services-carousel .element', 'block')
-menu()
+//menu()
 //forms()
 // sendForm()
 //slider()
-//scrollUp()
+scrollUp()
 
