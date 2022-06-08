d6766c9e3e7fff88895722ae88fb1e86528da7a8
YudakovVV <skiff2140@gmail.com>
2022-06-08 21:09:26 +0400

    create proj

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import modalHandlers from "./modules/modalHandlers"
 
 import menu from "./modules/menu"
 import forms from "./modules/forms"
-// import { sendForm } from "./modules/sendForm"
+import sendForm from "./modules/sendForm"
 import slider from "./modules/slider"
 import scrollUp from "./modules/scrollUp"
+import accordeon from "./modules/accordeon"
+import carousel from "./modules/carusel"
 
-
-// modalHandler('.mobile-menu .callback-btn', 'block')
+modalHandler('.mobile-menu .callback-btn', 'block')
 modalHandler('.header .callback-btn', 'block')
 modalHandler('.modal-close', 'none')
 modalHandler('.quest-section .button-services', 'block')
 modalHandlers('.services-carousel .element', 'block')
 menu()
 forms()
-// sendForm()
+sendForm()
 slider()
 scrollUp()
-
+accordeon()
+carousel()
